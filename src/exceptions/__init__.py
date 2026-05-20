b4703140cf89a51f0287f1bb357e631f38a19f95
AbianSL <abisanle@gmail.com>
2026-05-21 00:35:56 +0100

    easier to import

---
+++ src/exceptions/__init__.py
@@ -0,0 +1,7 @@
+from .llm_parse_exception import LLMParseException
+from .critical_system_exception import CriticalSystemException
+
+__all__ = [
+    "LLMParseException",
+    "CriticalSystemException",
+]
