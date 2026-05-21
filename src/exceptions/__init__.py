d6dfad0f4a9993e0e5d20f9796a63b10eb90c3c7
AbianSL <abisanle@gmail.com>
2026-05-21 10:16:12 +0100

    add trasientnetwork exception

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,7 +1,9 @@
 from .llm_parse_exception import LLMParseException
 from .critical_system_exception import CriticalSystemException
+from .transient_network_exception import TransientNetworkException
 
 __all__ = [
     "LLMParseException",
     "CriticalSystemException",
+    "TransientNetworkException",
 ]
