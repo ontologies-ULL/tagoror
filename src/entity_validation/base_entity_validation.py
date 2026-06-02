e457a1f2f7b35f96695137cc9a08d2ae4b55551f
AbianSL <abisanle@gmail.com>
2026-06-02 19:53:14 +0100

    fixing names

--- src/entity_validation/base_entity_validation.py
+++ src/entity_validation/base_entity_validation.py
@@ -4,13 +4,15 @@ Define the base class for all validation strategies
 
 from abc import ABC, abstractmethod
 
+from core.models import ExecutionSummary
+
 class ValidationStrategy(ABC):
     """
     Base interface to the strategies evaluators
     """
 
     @abstractmethod
-    async def evaluate(self, owl_entity: str) -> ValidationReport:
+    async def evaluate(self, owl_entity: str) -> ExecutionSummary:
         """
         Evaluate the entity using a type of strategy and generate a report about it.
 
@@ -18,6 +20,6 @@ class ValidationStrategy(ABC):
             owl_entity (str): Entity Owl to evaluate
 
         Returns:
-            ValidationReport: All data recolected for the final result 
+            ExecutionSummary: All data recolected for the final result 
         """
         pass
