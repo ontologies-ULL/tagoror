565d4f320e48054d3bdcd1c1232a36e96ccaf010
AbianSL <abisanle@gmail.com>
2026-06-25 20:15:51 +0100

    change to 4 space

--- src/core/pipeline/extractor.py
+++ src/core/pipeline/extractor.py
@@ -9,29 +9,29 @@ from typing import Any
 from owlready2 import get_ontology 
 
 class OntologyExtractor:
-  """
-  Converts ontology individuals into OntologyExtractionRecord instances.
-  """
-
-  _base_ontology_path: Path = Path(__file__).parent.parent / "ontologies" / "base_ontology.rdf"
-  _base_ontology = None
-
-  @classmethod
-  def extract(cls, file_path: str) -> list[Any]:
     """
-    Extract only ontology individuals.
+    Converts ontology individuals into OntologyExtractionRecord instances.
     """
-    if cls._base_ontology is None:
-      cls._base_ontology = cls._load_ontology(cls._base_ontology_path.resolve().as_uri())
 
-    ontology = OntologyExtractor._load_ontology(Path(file_path).resolve().as_uri())
-    return [individual for individual in ontology.individuals() if individual not in OntologyExtractor._base_ontology.individuals()]
+    _base_ontology_path: Path = Path(__file__).parent.parent / "ontologies" / "base_ontology.rdf"
+    _base_ontology = None
 
-  @staticmethod
-  def _load_ontology(file_path: str, ontology_loader=None):
-    """
-    Load an ontology file and return the parsed ontology object.
-    """
-    if ontology_loader is None:
-      ontology_loader = get_ontology
-    return ontology_loader(file_path).load()
+    @classmethod
+    def extract(cls, file_path: str) -> list[Any]:
+        """
+        Extract only ontology individuals.
+        """
+        if cls._base_ontology is None:
+            cls._base_ontology = cls._load_ontology(cls._base_ontology_path.resolve().as_uri())
+
+        ontology = OntologyExtractor._load_ontology(Path(file_path).resolve().as_uri())
+        return [individual for individual in ontology.individuals() if individual not in OntologyExtractor._base_ontology.individuals()]
+
+    @staticmethod
+    def _load_ontology(file_path: str, ontology_loader=None):
+        """
+        Load an ontology file and return the parsed ontology object.
+        """
+        if ontology_loader is None:
+            ontology_loader = get_ontology
+        return ontology_loader(file_path).load()
