e572022215ff06bc1711d90ba368fcb71baab59b
AbianSL <abisanle@gmail.com>
2026-06-30 03:17:02 +0100

    add a otel trace to get when is explote or not

--- src/serialization/serializers/turtle_serializer.py
+++ src/serialization/serializers/turtle_serializer.py
@@ -1,11 +1,15 @@
 from owlready2 import Thing, Ontology, default_world
 from serialization.base_serializer import BaseSerializer
 from rdflib import Graph, URIRef
+from opentelemetry.trace import Tracer, Status, StatusCode
 
 class TurtleSerializer(BaseSerializer):
     """
     Concrete implementation of BaseSerializer that serializes OWL individuals and ontologies into Turtle format.
     """
+
+    def __init__(self, tracer: Tracer) -> None:
+        self.tracer = tracer
 
     def process_ontology(self, ontology: Ontology) -> str:
         """
@@ -17,8 +21,19 @@ class TurtleSerializer(BaseSerializer):
         Returns:
             A Turtle string representation of the ontology.
         """
-        graph = default_world.as_rdflib_graph(ontology)
-        return graph.serialize(format="turtle")
+        with self.tracer.start_as_current_span("serialize_ontology_to_turtle") as span:
+            span.set_attribute("ontology_iri", ontology.base_iri)
+            span.set_attribute("ontology_name", ontology.name)
+            try:
+                graph = default_world.as_rdflib_graph(ontology)
+                result = graph.serialize(format="turtle")
+
+                span.set_status(Status(status_code=StatusCode.OK))
+                return result
+            except Exception as error:
+                span.set_status(Status(status_code=StatusCode.ERROR, description=str(error)))
+                
+                raise
 
     def process_individual(self, individual: Thing) -> str:
         """
@@ -30,11 +45,23 @@ class TurtleSerializer(BaseSerializer):
         Returns:
             A Turtle string representation of the individual.
         """
-        full_graph = default_world.as_rdflib_graph()
-        individual_graph = Graph()
-        subject_iri = URIRef(individual.iri)
+        with self.tracer.start_as_current_span("serialize_individual_to_turtle") as span:
+            span.set_attribute("individual_iri", individual.iri)
+            span.set_attribute("individual_name", individual.name)
+            try:
+                subject_iri = URIRef(individual.iri)
+                full_graph = default_world.as_rdflib_graph()
+                individual_graph = Graph()
+                
+                for entity, relationship, target in full_graph.triples((subject_iri, None, None)):
+                    individual_graph.add((entity, relationship, target))
+                
+                result = individual_graph.serialize(format="turtle", subject=subject_iri, graph=full_graph)
+                span.set_status(Status(status_code=StatusCode.OK))
+                return result
+            except Exception as error:
+                span.set_status(Status(status_code=StatusCode.ERROR, description=str(error)))
+                
+                raise
         
-        for s, p, o in full_graph.triples((subject_iri, None, None)):
-            individual_graph.add((s, p, o))
             
-        return individual_graph.serialize(format="turtle")
